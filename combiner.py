0dd29ea8121248434dea5aba978efbf7a7575e32
yaoxy007 <yaoxueye@gmail.com>
2019-04-04 04:49:00 +1100

    asdf

--- combiner.py
+++ combiner.py
@@ -15,22 +15,17 @@ def combiner():
     data = read_map_output(sys.stdin)
     nums_of_category = {}
     ids_in_country = {}
-    output = ""
 
     for category, vid, country in data:
         nums_of_category.setdefault(category,set()).add(vid)
         ids_in_country.setdefault(vid,set()).add(country)
     
     for cat,idset in nums_of_category.items():
-        for ids in ids_in_country:
+        for ids,countries in ids_in_country.items():
             if ids in list(idset):
-                print("{},{},{}".format(cat,ids,list(ids_in_country[ids])))
-
-
-    # for cat in nums_of_category:
-    #     for ids in ids_in_country:
-    #         if(ids in nums_of_category[cat]):
-    #             print("{},{},{}".format(cat,ids,list(ids_in_country[ids])))
+                print("{},{},{}".format(cat,ids,list(countries)))
+            else:
+                continue
     
 
 
