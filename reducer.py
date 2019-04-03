c56f84e8a593a0d8cb8939ed9f05c1e293e8d702
yaoxy007 <yaoxueye@gmail.com>
2019-04-04 03:11:32 +1100

    12

--- reducer.py
+++ reducer.py
@@ -17,15 +17,28 @@ def reducer():
     id_count = 0
     country_count = 0
     output = ""
+    c={}
     for category,ids,countries in data:
         country=countries.strip().split(",")
-        if current_category != category:
-            if current_category!="":
-                output = "{},{}".format(current_category,country_count/id_count)
-                print(output.strip())
+        if not current_category:
             current_category=category
-        country_count += len(country)
-        id_count += 1
+        elif category!=current_category:
+            total_country = sum(map(lambda x:len(x),c.values()))
+            total_ids = len(c)
+            print("{},{}".format(category,total_country/total_ids))
+        c[ids] = c.get(ids,set()) | country
+    
+    total_country = sum(map(lambda x:len(x),c.values()))
+    total_ids = len(c)
+    print("{},{}".format(category,total_country/total_ids))
+
+        # if current_category != category:
+        #     if current_category!="":
+        #         output = "{},{}".format(current_category,country_count/id_count)
+        #         print(output.strip())
+        #     current_category=category
+        # country_count += len(country)
+        # id_count += 1
 
     # output = "{},{}".format(current_category,country_count/id_count)
     # print(output.strip())
