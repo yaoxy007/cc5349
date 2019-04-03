51a2aaab01fdcf59fd5cb40adb36b49ce99b3b25
yaoxy007 <yaoxueye@gmail.com>
2019-04-04 00:26:44 +1100

    4

--- combiner.py
+++ combiner.py
@@ -20,13 +20,14 @@ def combiner():
     for category, vid, country in data:
         nums_of_category.setdefault(category,set()).add(vid)
         ids_of_category=nums_of_category.values()
-        ids_in_country.setdefault(ids_of_category,set()).add(country)
+        for ids in ids_of_category:
+            ids_in_country.setdefault(ids,set()).add(country)
     
     for cat in nums_of_category:
         for ids in ids_in_country:
             if(ids in nums_of_category[cat]):
                 print("{},{},{}".format(cat,ids,ids_in_country[ids]))
-
+    
 
 
 if __name__ == "__main__":
