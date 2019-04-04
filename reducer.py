167c35604a13ea113a77f3e7bb4f54fb772f62ee
yaoxy007 <yaoxueye@gmail.com>
2019-04-04 21:12:44 +1100

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -21,15 +21,6 @@ def reducer():
         country=ids_and_countries.strip().split(":")[1].strip("[").strip("]")
         country_list = country.split(",")
         country=set(country_list)
-        # if current_category == "":
-        #     current_category = category
-        # elif current_category != category:
-        #         total_country = sum(map(lambda x:len(x),c.values()))
-        #         total_ids = len(c)
-        #         print("{key}, {val}".format(key=current_category,val=round(total_country/total_ids,2)))
-        #         c.clear()
-        #         current_category=category
-        # c[ids] = c.get(ids,set()) | country
         
         if(current_category == category):
             c[ids] = c.get(ids,set()) | country
@@ -47,46 +38,7 @@ def reducer():
     if current_category!="":
         total_country = sum(map(lambda x:len(x),c.values()))
         total_ids = len(c)
-        print("{key}\t{val}".format(key=current_category,val=round(total_country/total_ids,2)))
+        print("{key}, {val}".format(key=current_category,val=round(total_country/total_ids,2)))
 
-# import sys
-
-
-# def read_combiner_output(line):
-#     category, parts = line.strip().split('\t')
-#     parts = parts.split(':')
-#     video_id = parts[0]
-#     country=parts[1].strip("[").strip("]")
-#     country_list = country.split(",")
-#     country=set(country_list)
-#     return category, video_id, country
-
-
-# def output(category, country_dict):
-#     total = sum(map(lambda x: len(x), country_dict.values()))
-#     num_of_videos = len(country_dict)
-#     print('%s: %s' % (category, total / num_of_videos))
-
-
-# def reducer():
-#     """
-#     Input: category    video_id,country_1,country_2,...,country_n
-#     Output: category: 1.4
-#     """
-#     current_category = ''
-#     trending_countries = {}
-#     for line in sys.stdin:
-#         category, video_id, country_set = read_combiner_output(line)
-#         if not current_category:
-#             current_category = category
-#         elif category != current_category:
-#             # receiving data with new category means last category finished
-#             output(current_category, trending_countries)
-#             trending_countries.clear()
-#             current_category = category
-#         # get previous country list if exists and add new countries in it
-#         trending_countries[video_id] = trending_countries.get(video_id, set()) | country_set
-#     output(current_category, trending_countries)
-    
 if __name__ == "__main__":
     reducer()
