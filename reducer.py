4a220057827a6c4d14c75ba5dc53f9b0dabb0930
yaoxy007 <yaoxueye@gmail.com>
2019-04-04 17:49:21 +1100

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -13,31 +13,32 @@ def read_combiner_output(output):
 
 def reducer():
     data = read_combiner_output(sys.stdin)
-    current_category = ""
-    c={}
+    print(data)
+    # current_category = ""
+    # c={}
 
-    for category,ids_and_countries in data:
-        ids = ids_and_countries.strip().split(":")[0]
-        country=ids_and_countries.strip().split(":")[1]
-        country_list = country.split(",")
-        country=set(country_list)
+    # for category,ids_and_countries in data:
+    #     ids = ids_and_countries.strip().split(":")[0]
+    #     country=ids_and_countries.strip().split(":")[1]
+    #     country_list = country.split(",")
+    #     country=set(country_list)
         
-        if not current_category:
-            current_category=category
-        elif category!=current_category:
-            total_country = sum(map(lambda x:len(x),c.values()))
-            total_ids = len(c)
+    #     if not current_category:
+    #         current_category=category
+    #     elif category!=current_category:
+    #         total_country = sum(map(lambda x:len(x),c.values()))
+    #         total_ids = len(c)
    
-            print("{key},{val}".format(key=category,val="%.2f" % total_country/total_ids))
-            c.clear()
-            current_category=category
-            total_country=0
-            total_ids=0
-        c[ids] = c.get(ids,set()) | country
+    #         print("{key},{val}".format(key=category,val="%.2f" % total_country/total_ids))
+    #         c.clear()
+    #         current_category=category
+    #         total_country=0
+    #         total_ids=0
+    #     c[ids] = c.get(ids,set()) | country
 
-    total_country = sum(map(lambda x:len(x),c.values()))
-    total_ids = len(c)
-    print("{key},{val}".format(key=category,val="%.2f" % total_country/total_ids))
+    # total_country = sum(map(lambda x:len(x),c.values()))
+    # total_ids = len(c)
+    # print("{key},{val}".format(key=category,val="%.2f" % total_country/total_ids))
 
     
 if __name__ == "__main__":
