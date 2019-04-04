98c61c88a7a214c3633716025eb435d4a16208af
yaoxy007 <yaoxueye@gmail.com>
2019-04-04 17:23:24 +1100

    asdas

--- reducer.py
+++ reducer.py
@@ -15,7 +15,8 @@ def reducer():
     data = read_combiner_output(sys.stdin)
     current_category = ""
     c={}
-  
+    total_country=0
+    total_ids=0
     for category,ids_and_countries in data:
         ids = ids_and_countries.strip().split(":")[0]
         country=ids_and_countries.strip().split(":")[1]
@@ -26,14 +27,16 @@ def reducer():
         elif category!=current_category:
             total_country = sum(map(lambda x:len(x),c.values()))
             total_ids = len(c)
-            print("{},{}".format(category,total_country/total_ids))
+            print("{key},{val}".format(key=category,val="%.2f" % total_country/total_ids))
             c.clear()
             current_category=category
         c[ids] = c.get(ids,set()) | country
     
-    total_country = sum(map(lambda x:len(x),c.values()))
-    total_ids = len(c)
-    print("{},{}".format(category,total_country/total_ids))
+    total_country += len(country)
+    total_ids += 1
+    # total_country = sum(map(lambda x:len(x),c.values()))
+    # total_ids = len(c)
+    print("{key},{val}".format(key=category,val="%.2f" % total_country/total_ids))
 
     
 if __name__ == "__main__":
