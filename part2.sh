7a97ad396c5a7104d7c71905573ddaf85822e898
root <root@ip-172-31-27-205.ap-southeast-2.compute.internal>
2019-04-07 07:31:13 +0000

    finalized part2

--- part2.sh
+++ part2.sh
@@ -3,7 +3,7 @@
 spark-submit \
     --master local[4] \
     part2.py \
-    --input file:///home/hadoop/comp5349/cc5349/a1/ \
+    --input file:///home/hadoop/comp5349/cc5349/lab_commons/a1_data/ \
     --output file:///home/hadoop/comp5349/cc5349/cc5349/
 	 
 
